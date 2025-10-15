c9098bfe8981fd306d2775aa1ff512e70beb0b12
horllydreycz <oluwadaretitus3@gmail.com>
2025-10-15 11:46:09 +0100

    Fix variable casing for dataContainer in fetchUserData function

--- fetch-data.js
+++ fetch-data.js
@@ -1,19 +1,19 @@
 async function fetchUserData() {
   const apiUrl = "https://jsonplaceholder.typicode.com/users";
-  const DataContainer = document.getElementById("api-data");
+  const dataContainer = document.getElementById("api-data");
   try {
     const response = await fetch(apiUrl);
     const users = await response.json();
-    DataContainer.innerHTML = "";
+    dataContainer.innerHTML = "";
     const userList = document.createElement("ul");
     array.forEach((users) => {
       const list = document.createElement("li");
       list.textContent = userList;
       userList.appendChild(list);
     });
-    DataContainer.appendChild(userList);
+    dataContainer.appendChild(userList);
   } catch (error) {
-    DataContainer.innerHTML = "Failed to load user data.";
+    dataContainer.innerHTML = "Failed to load user data.";
   }
 }
 document.addEventListener("DOMContentLoaded", fetchUserData);
